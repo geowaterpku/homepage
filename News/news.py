d04c420bd2e241b125a0e9dd46a6c43b28d917ef
zhangkai8 <zhangkai8@lixiang.com>
2022-10-29 22:34:56 +0800

    add news

---
+++ News/news.py
@@ -0,0 +1,63 @@
+from collections import OrderedDict
+
+news = OrderedDict()
+news['2022'] = [
+	"""<b>实验室新闻：I am always looking for highly motivated PhD/master/undergraduate students and Postdoctoral Fellows to work in my lab. We study the geography of global inland waters, and we develop physically-based models, data-driven methods, and geographic information data infrastructures to improve our fundamental understanding of the river-climate and river-human relationships. Please reach out to <a href="peironglinlin@pku.edu.cn">peironglinlin@pku.edu.cn</a> if you are interested in working <span style="color:#0F33E9">@ GeoWater lab @ PKU</span></b>.<br><br>
+	🧑‍💻 <span style="color:#E9A70F"><b>近期职位 | Recent openning: a Postdoc or Research Assistant to start in Dec. 2022 (other times negotiable) to work on a funded project on global floodplain mapping.</b></span> (position requirement: good computational/coding skills in handling big data, teamwork spirit, good academic writing/speaking skills; trackable academic records; position plus: strong curiosity for the unknown). """,
+
+	"""<b>2022-10: 学术服务与奖励 | Dr. Lin is honorded to be selected for the "College GIS Forum Young Scientist Award" (<span style="color:#0F33E9">高校GIS新锐奖</span>) ! Dr. Lin will give a talk at the College GIS Forum on 10/30. (See the <a href="https://mp.weixin.qq.com/s/by7C1Izgg3zBoM6MruE32g">News</a>)</b>""",
+
+	"""<b>2022-09: 实验室成员将参加年底AGU秋季会议以及年底三校国际地理信息论坛 | It's great to see our members from the GeoWater lab submitted their abstracts to the <span style="color:#0F33E9">AGU Fall Meeting</span>, and the <span style="color:#0F33E9">IGWG 2022 meeting hosted by the HK PolyU, PKU, and Wuhan University</span>. Details below:</b><br>
+		&ensp;&ensp;&ensp;&ensp;👩 Ziyun Yin (first-year PhD student at PKU): oral talk at AGU<br>
+		&ensp;&ensp;&ensp;&ensp;🤵‍♂️ Xiongyong Lei (first-year PhD student at PKU): oral talk at IGWG<br>
+		&ensp;&ensp;&ensp;&ensp;👩 Meng Ding (visiting student from Kansas State U): oral talk at AGU<br>
+		&ensp;&ensp;&ensp;&ensp;🤵‍♂️ Kaihao Zheng (senior-year undergrads): poster at AGU/oral talk at IGWG<br>
+		&ensp;&ensp;&ensp;&ensp;🤵‍♂️ Zimin Yuan (senior-year undergrads): talk at IGWG""",
+
+	"""<b>2022-09：科研项目 | As PI, Dr. Lin's proposed research project on global floodplain mapping was funded by <span style="color:#0F33E9">the CBAS international center</span>.</b><br>
+		&ensp;&ensp;&ensp;&ensp;⭐ We will work on improving our understanding of the floodplain and their changes/risks in the coming 1.5 years.""",
+
+	"""<b>2022-09: 实验室新闻 | Welcome to <span style="color:#0F33E9">Ziyun Yin</span> and <span style="color:#0F33E9">Xiongyong Lei</span> for officially joining the GeoWater lab as PhD students.</b>""",
+
+	"""<b>2022-09: 会议组织 | Convened a session on "Climate Adaptation and Mitigation to Changing Water Availability and Water Hazards" in <span style="color:#0F33E9">FBAS 2022 International Forum</span>. We are honored to have invited Prof. Qiuhong Tang from CAS, Prof. Yue Qin from Peking University, Prof. Hong Xuan Do from Nong Lam University, Dr. Andrew Smith from Fathom, and Dr. Menaka Revel from University of Tokyo to join us as the invited speaker! </b><br>
+		&ensp;&ensp;&ensp;&ensp;📹 See the session details and recorded video here: <a href="https://fbas2022.scimeeting.cn/cn/web/program/14428">https://fbas2022.scimeeting.cn/cn/web/program/14428</a>""",
+
+	"""<b>2022-08: 会议组织 |  I convened a session named "New methods in hydrologic monitoring and modeling" at the <span style="color:#0F33E9">9th summer meeting for CYWater</span>!</b> (together with Gang Zhao from Stanford, Hui Lu from Tsinghua University, Xiaogang He from National University of Singapore, and Yao Li from Xinan University).""",
+
+	"""<b>2022-05: 实验室新闻 | Undergraduate student <span style="color:#0F33E9">Kaihao Zheng</span> won the First Prize in the Challenging Cup competition for his research work on levee detection advised by Dr. Lin.</b> This competition consists of mostly masters or PhD students (can be in teams or single player), with less than 10% undergrads participating. Kaihao completed the competition on his own and won the award. <b><span style="color:#0F33E9">Big congratulations!</span></b>""",
+
+	"""<b>2022-04-25: 科研成果 | Co-authored paper published in <i><span style="color:#0F33E9">Nature Sustainability</span></i>! </b> Another work examining our rivers using an earth system science view, and a collaborative effort with the DryRiverRCN team, led by Corey Krabbenhoft. See article link here: <a href="https://www.nature.com/articles/s41893-022-00873-0">https://www.nature.com/articles/s41893-022-00873-0</a>""",
+
+	"""<b>2022-04: 学术服务与奖励 | Water Resources Research Editors has recommended that I receive <span style="color:#0F33E9">the 2021 Editors’ Citation for Excellence in Refereeing for <i>Water Resources Research</i></span>! Honored to receive such positive feedbacks as a reviewer.</b>""",
+
+	"""<b>2022-01: 学术服务与奖励 | Joining Journal of Remote Sensing as a Young Editorial Board Member.</b>""",
+
+	"""<b>2022-1-17: 实验室招聘 | The prestigious <span style="color:#0F33E9">Boya Postdoc Fellowship application</span> is open this year! </b><br>
+	&ensp;&ensp;&ensp;&ensp;⚡ Check out the application notice here: <a href="https://postdocs.pku.edu.cn/tzgg/134996.htm">https://postdocs.pku.edu.cn/tzgg/134996.htm</a> <br>
+	&ensp;&ensp;&ensp;&ensp;⚡ If you are interested in working at PKU as a postdoc and <span style="color:#0F33E9"><i>expand your research topics on using remote sensing/GIS to addressing water-energy-food-ecology nexus problems</i></span>, you may have the opportunity to work collaboratively with several faculty members in our department (Institute of Remote Sensing and GIS @ PKU), with myself being the major advisor. <br>
+	&ensp;&ensp;&ensp;&ensp;⚡ Note two deadlines are in March and September, respectively. <br>
+	&ensp;&ensp;&ensp;&ensp;⚡ Interested candidates may email <a href="peironglinlin@pku.edu.cn">peironglinlin@pku.edu.cn</a> and a brief research statement and CV would be helpful."""
+]
+
+news['2021'] = [
+	"""<b>2021: 科研成果 | Several papers that I co-authored are published recently. Check out:</b><br>
+	&ensp;&ensp;&ensp;&ensp;📖 Feng, D., C. Gleason, <b><u>P. Lin</b></u>, X. Yang, M. Pan, Y. Ishitsuka: Recent changes to Arctic river discharge. <i>Nature communications. </i><a href="https://www.nature.com/articles/s41467-021-27228-1">link</a>.<br>
+	&ensp;&ensp;&ensp;&ensp;📖 Gao, S., Z. Li, M. Chen, <b><u>P. Lin</b></u>, Z. Hong, D. Allen, T. Neeson, Y. Hong: Spatiotemporal variability of global river extent and the natural driving factors revealed by decades of Landsat observations, GRACE gravimetry observations, and land surface model simulations. <i>Remote Sensing of Environment. </i><a href="https://www.sciencedirect.com/science/article/abs/pii/S0034425721004454">link</a><br>
+	&ensp;&ensp;&ensp;&ensp;📖 Riggs, R., G. Allen, C. David, <b><u>P. Lin</b></u>, M. Pan, X. Yang, C. Gleason: RODEO: An algorithm and Google Earth Engine application for river discharge retrieval from Landsat, <i>Environmental Modelling & Software</i>. </i><a href="https://www.sciencedirect.com/science/article/abs/pii/S1364815221002966">link</a>""",
+
+	"""<b>2021-12/18-19: 会议组织 | I chaired <span style="color:#0F33E9">the 3rd International Graduate Workshop on Geoinformatics (IGWG)</span> for a two-day event.</b><br>
+	This workshop is co-organized by Peking University, Wuhan University, and the Hong Kong Polytechnic University. This year PKU is the leading organizer. 59 students from 14 universities presented their work orally, judged by 10 faculty advisors we invited from PKU, WHU, PolyU, and U Minnesota.""",
+
+	"""<b>2021-07-31 to 08-01: 会议组织 |  I convened a session named <span style="color:#0F33E9">"New methods in hydrologic monitoring and modeling" at the 9th summer meeting for CYWater!</span></b> (together with Hui Lu from Tsinghua University, Xiaogang He from National University of Singapore, and Yanhong Gao from CAS).""",
+
+	"""<b>2021-07-27: 科研报告 | I gave an invited talk at the <span style="color:#0F33E9">"Quantitative Remote Sensing Summer School"</span></b> , one of the most influential summer programs at Peking University for the past 20 years. The topic is on the "Role of geospatial information techniques on the global high-resolution river discharge modeling".""",
+
+	"""<b>2021-06-30: 科研报告 | I gave an invited talk at <span style="color:#0F33E9">Hydro90</span>.</b> 200+ audience showed up, with a lot of great questions. See more about Hydro90 on <a href="https://twitter.com/hydro90er?lang=en">Twitter</a>.""",
+
+	"""<b>2021 Summer: 实验室新闻 | <span style="color:#0F33E9">I will join Peking University as a tenure-track Assistant Professor!</span> Please contact <a href="peironglinlin@pku.edu.cn">peironglinlin@pku.edu.cn</a> for further correspondence</b>, or if you are interested in joining us at PKU! (PKU is located in Beijing, China, and the program is from Institute of Remote Sensing and GIS at School of Earth and Space Sciences. We will use an Earth System Science approach to study global water problems @ GeoWater lab).""",
+
+	"""<b>2021-01-28: 科研成果 | I published three first-author papers since joining Princeton as a postdoc in June 2018.</b> Check out Lin et al. (2019; WRR), Lin et al. (2020; GRL) and Lin et al (2021; Scientific Data). These three papers all focused on global rivers (e.g., discharge, bankfull width, and drainage density) and would lay the foundation for many ensuing global river studies!""",
+
+	"""<b>2020-12-09: 会议组织 | I, together with George Allen (TAMU), Xiao Yang (UNC), Rodrigo Paiva convened <span style="color:#0F33E9">a AGU session on "Remote sensing and modeling of global rivers from the headwaters to the ocean"</span>.</b>"""
+]
+
